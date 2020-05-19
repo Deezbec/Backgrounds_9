c14c872b01b511e3ae92300578030cd89abef92d
Deezbec <greger.ert@gmail.com>
2020-05-19 11:20:30 +0300

    Добавил фоновой 6.1

---
+++ Фоновая 5.2/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Фоновая_5._2
+{
+    enum DriveUnitType { manual, steam, hydraulic, pneumatic, internalCombustionEngine, electric };
+    class Truck 
+    {
+        protected string name;
+        protected int truckMass;
+        protected int lenLoss;
+        protected int maxMass;
+        protected int goodsMass;
+        protected Truck()
+        {
+            this.name = "DUBNA";
+            this.truckMass = 500;
+            this.lenLoss = 10000;
+            this.maxMass = 1000;
+            this.goodsMass = 967;
+        }
+        protected Truck(string name, int truckMass, int lenLoss, int maxMass, int goodsMass)
+        {
+            this.name = name;
+            this.truckMass = truckMass;
+            this.lenLoss = lenLoss;
+            this.maxMass = maxMass;
+            this.goodsMass = goodsMass;
+        }
+        protected int GetAllMass { get { return goodsMass + truckMass; } }
+        protected string GetName { get { return this.name; } }
+        protected int GetGoodsMass { get { return this.goodsMass; } set { this.goodsMass = value; } }
+        protected void AllTrucksInf(out string name, out int truckMass, out int lenLoss, out int maxMass, out int goodsMass)
+        {
+            name = this.name;
+            truckMass = this.truckMass;
+            lenLoss = this.lenLoss;
+            maxMass = this.maxMass;
+            goodsMass = this.goodsMass;
+        }
+        protected int MaxLengthNow() { return this.lenLoss - GetAllMass / 100; }
+    }
+    class WheelsT : Truck
+    {
+        protected int wheelCount;
+        protected DriveUnitType UnitType;
+        public WheelsT () : base()
+        {
+            wheelCount = 8;
+            UnitType = (DriveUnitType)1;
+        }
+        public WheelsT(string name, int truckMass, int lenLoss, int maxMass, int goodsMass, int wheelCount, int UnitType) : base(name, truckMass, lenLoss, maxMass, goodsMass)
+        {
+            this.wheelCount = wheelCount;
+            this.UnitType = (DriveUnitType)UnitType;
+        }
+        new public int MaxLengthNow()
+        {
+            if (wheelCount < 4) return 0;
+            return base.MaxLengthNow();
+        }
+        public void AllTrucksInf(out string name, out int truckMass, out int lenLoss, out int maxMass, out int goodsMass, out int wheelCount, out DriveUnitType UnitType)
+        {
+            base.AllTrucksInf(out name, out truckMass, out lenLoss, out maxMass, out goodsMass);
+            wheelCount = this.wheelCount;
+            UnitType = this.UnitType;
+        }
+    }
+    class FreightTrain : WheelsT
+    {
+        protected int countWagon;
+        public FreightTrain() : base()
+        {
+            countWagon = 75;
+        }
+        public FreightTrain(string name, int truckMass, int lenLoss, int maxMass, int goodsMass, int wheelCount, int UnitType, int countWagon) : base(name, truckMass, lenLoss, maxMass, goodsMass, wheelCount, UnitType)
+        {
+            this.countWagon = countWagon;
+        }
+        public void AllTrucksInf(out string name, out int truckMass, out int lenLoss, out int maxMass, out int goodsMass, out int wheelCount, DriveUnitType UnitType, out int countWagon)
+        {
+            base.AllTrucksInf(out name, out truckMass, out lenLoss, out maxMass, out goodsMass, out wheelCount, out UnitType);
+            countWagon = this.countWagon;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Фоновая 6.1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Фоновая_6._1
+{
+    public partial class Form1 : Form
+    {
+        private int WidthButton; // К сожалению, не нашел другого способа сохранить длину кнопки
+        public Form1()
+        {
+            InitializeComponent();
+            WidthButton = button1.Size.Width;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button2.Location.X - 10 > 0) button2.Left = button2.Location.X - 10;
+            else 
+            {
+                if (button2.Location.X == 0) button2.Left = Form1.ActiveForm.Size.Width - button2.Size.Width - 16 ; //вычитаю рамки окна, каждая равна 8 
+                else button2.Left = 0; 
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button1.Size.Width - 2 > 0) button1.Width = button1.Size.Width - 2;
+            else button1.Width = WidthButton;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
